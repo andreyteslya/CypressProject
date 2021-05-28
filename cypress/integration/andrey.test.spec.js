9406aa71b2f5fb1e7fce094ccf8683b7e32b5a3c
andreyteslya <teslyauar@gmail.com>
2021-05-28 09:49:38 +0300

    test33

--- cypress/integration/andrey.test.spec.js
+++ cypress/integration/andrey.test.spec.js
@@ -1,13 +1,29 @@
+/// <reference types="cypress" />
 // it.only("By ID", ()=>{
 //     cy.visit("https://next.privat24.ua/")
 // });
 
-it("By Tag", ()=>{
-    cy.visit("https://facebook.com/")
-    cy.get('[name="pass"]')
-});
+//const { it } = require("mocha");
 
-it.only("By Tag", ()=>{
-    cy.visit("https://facebook.com/")
-    cy.get('[data-testid="open-registration-form-button"][role="button"]')
-});
+//it("By Tag", ()=>{
+ //   cy.visit("https://facebook.com/")
+ //   cy.get('[name="pass"]')
+//});
+
+//it.("By Tag", ()=>{
+ //   cy.visit("https://facebook.com/")
+//    cy.get('[data-testid="open-registration-form-button"][role="button"]')
+//});
+
+//it.("By Tag", ()=>{
+//    cy.visit("https://facebook.com/")
+ //   cy.contains('чоловік')
+//});
+
+it("By Compose", ()=>{
+    cy.visit('https://pre.beeble.com/')
+    cy.get('[name="secure-username"]').type('test_qa@beeble.com')
+    cy.get('#signin_pass').type('Qwertyasdf123!')
+    cy.get('#sigin_button').click()
+    cy.get('#headerActionsCompose').click()
+})
